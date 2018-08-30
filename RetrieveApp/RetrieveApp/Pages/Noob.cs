cd33aecbd317f3809c2b945f21799380a89d46c6
mohanad oweidat <mohanad.oweidat@hotmail.com>
2018-08-30 11:49:37 +0200

    I Added a stacklayout

---
+++ RetrieveApp/RetrieveApp/Pages/Noob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetrieveApp.Pages
+{
+   public class Noob
+    {
+        public void test()
+        {
+            Console.WriteLine("My name is a bozr");
+        }
+    }
+}
