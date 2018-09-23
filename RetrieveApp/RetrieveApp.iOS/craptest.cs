6ce16c4201633454a17f1e76408380989914915b
elev <mohanad.owidat@hotmail.com>
2018-09-23 16:57:47 +0200

    Craptest class

---
+++ RetrieveApp/RetrieveApp.iOS/craptest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RetrieveApp.iOS
+{
+    public class craptest
+    {
+        public craptest()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Osama Noob");
+            }
+        }
+    }
+}
