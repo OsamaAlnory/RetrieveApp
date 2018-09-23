aa2c769553c042106513efff2e2206500d6a041d
elev <mohanad.owidat@hotmail.com>
2018-09-23 16:51:48 +0200

    i added a class

---
+++ RetrieveApp/RetrieveApp/Elements/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace RetrieveApp.Elements
+{
+    public class test
+    {
+        public test()
+        {
+            int x = 10;
+            int y = 3;
+            int summa = x + y;
+            Console.WriteLine(summa);
+        }
+    }
+}
