bf6f2cfd659504c55e20a318eafd817e1e2e396f
mohanad oweidat <mohanad.oweidat@hotmail.com>
2018-10-10 08:54:19 +0200

    ss

--- RetrieveApp/RetrieveApp.Android/SplashActivity.cs
+++ RetrieveApp/RetrieveApp.Android/SplashActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace RetrieveApp.Droid
 {
-    [Activity(Theme ="@style/Theme.Splash",MainLauncher = true,  NoHistory =true  )]
+    [Activity(Icon = "@mipmap/icon", Theme ="@style/Theme.Splash",MainLauncher = true,  NoHistory =true  )]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
